141547eb2f61eef273474c5e81fba71247830dba
Justin Dietert <justin.dietert@gmail.com>
2015-02-26 22:16:23 -0600

    style adjustments and feature additions

--- public/js/simon_two.js
+++ public/js/simon_two.js
@@ -1,122 +1,140 @@
-var userSequence = [];
-var simonSequence = [];
+(function() {
 
-var tiles = document.getElementsByClassName('tile');
-var startButton = document.getElementById('start');
+    'use strict';
 
-startButton.addEventListener('click', start, false);
+    var userSequence = [];
+    var simonSequence = [];
 
-function start() {
-    simonSequence = [];
-    newRound();
-}
+    var tiles = document.getElementsByClassName('tile');
+    var startButton = document.getElementById('start');
 
-function newRound() {
-    userSequence = [];
-    addRandomSquareToSequence();
-    animate();
-}
+    startButton.addEventListener('click', start, false);
 
-function addRandomSquareToSequence() {
-    var random = Math.floor(Math.random() * 4);
-    simonSequence.push(tiles[random].id);
-}
-
-function animate() {
-    disableInput();
-
-    // update score on screen 
-    // document.getElementById('round').innerText = "Round: " + simonSequence.length;
-
-    var i = 0;
-
-    var intervalId = setInterval(function(){
-        lightUp(document.getElementById(simonSequence[i]));
-
-        i++;
-
-        if (i >= simonSequence.length) {
-            clearInterval(intervalId);
-
-            enableInput();
-        }
-    }, 800);
-}
-
-function lightUp(element) {
-    element.style.opacity = "1";
-
-    var fadeoutTimerId = setTimeout(function(){
-        element.style.opacity = "0.5";
-    }, 500);
-}
-
-function compareSequences() {
-    var sequenceError = false;
-
-    for (var i = 0; i < userSequence.length; i ++) {
-        if (simonSequence[i] == undefined || simonSequence[i] != userSequence[i]) {
-            sequenceError = true;
-            break;
-        }
-    }
-
-    if (sequenceError) {
-        gameOver();
-    } else if (userSequence.length == simonSequence.length) {
+    function start() {
+        simonSequence = [];
         newRound();
     }
-}
 
-function gameOver() {
-    location.reload(true);
-
-    confirm("Game over. Play again?");
-
-    if(confirm) {
-        start();
+    function newRound() {
+        userSequence = [];
+        addRandomTileToSequence();
+        animate();
     }
-}
 
-
-function userClick() {
-    var userChoice = this.id;
-
-    userSequence.push(userChoice);
-
-    compareSequences();
-}
-
-
-function enableInput() {
-    document.getElementById('0').addEventListener('click', userClick, false);
-    document.getElementById('1').addEventListener('click', userClick, false);
-    document.getElementById('2').addEventListener('click', userClick, false);
-    document.getElementById('3').addEventListener('click', userClick, false);
-
-    for (var i = 0; i < tiles.length; i++) {
-        tiles[i].addEventListener('mousedown', userDown, false);
-        tiles[i].addEventListener('mouseup', userUp, false);
+    function addRandomTileToSequence() {
+        var random = Math.floor((Math.random() * 4));
+        simonSequence.push(tiles[random].id);
     }
-}
 
-function disableInput() {
-    document.getElementById('0').removeEventListener('click', userClick, false);
-    document.getElementById('1').removeEventListener('click', userClick, false);
-    document.getElementById('2').removeEventListener('click', userClick, false);
-    document.getElementById('3').removeEventListener('click', userClick, false);
+    function animate() {
+        disableUserClick();
 
-    for (var i = 0; i < tiles.length; i++) {
-        tiles[i].removeEventListener('mousedown', userDown, false);
-        tiles[i].removeEventListener('mouseup', userUp, false);
+        // update score on screen 
+        document.getElementById('start').innerHTML = "Round: " + simonSequence.length;
+        document.getElementById('start').classList.add('round-text');
+
+        // tell user to watch sequence
+        document.getElementById('prompt-text').classList.remove('repeat-sequence');
+        document.getElementById('prompt-text').innerHTML = "Watch the sequence.";
+        document.getElementById('prompt-text').classList.add('watch-sequence');
+
+        var i = 0;
+
+        var intervalId = setInterval(function(){
+            lightUp(document.getElementById(simonSequence[i]));
+
+            i++;
+
+            if (i >= simonSequence.length) {
+                clearInterval(intervalId);
+
+                enableUserClick();
+            }
+        }, 800);
     }
-}
 
-function userDown(event) {
-    event.target.style.opacity = "1";
-}
+    function lightUp(element) {
+        element.style.opacity = "1";
 
-function userUp(event) {
-    event.target.style.opacity = "0.5";
-}
+        var fadeoutTimerId = setTimeout(function(){
+            element.style.opacity = "0.3";
+        }, 500);
+    }
 
+    function compareSequences() {
+        var sequenceError = false;
+
+        for (var i = 0; i < userSequence.length; i ++) {
+            if (simonSequence[i] == undefined || simonSequence[i] != userSequence[i]) {
+                sequenceError = true;
+                break;
+            }
+        }
+
+        if (sequenceError) {
+            gameOver();
+        } else if (userSequence.length == simonSequence.length) {
+            newRound();
+        }
+    }
+
+    function gameOver() {
+        disableUserClick();
+        // location.reload(true);
+
+        // confirm("Game over. Play again?");
+
+        // if(confirm) {
+        //     start();
+        // }
+    }
+
+
+    function userClick() {
+        var userChoice = this.id;
+
+        userSequence.push(userChoice);
+
+        compareSequences();
+    }
+
+
+    function enableUserClick() {
+        document.getElementById('0').addEventListener('click', userClick, false);
+        document.getElementById('1').addEventListener('click', userClick, false);
+        document.getElementById('2').addEventListener('click', userClick, false);
+        document.getElementById('3').addEventListener('click', userClick, false);
+
+        for (var i = 0; i < tiles.length; i++) {
+            tiles[i].addEventListener('mousedown', userDown, false);
+            tiles[i].addEventListener('mouseup', userUp, false);
+        }
+
+        // tell user to repeat sequence.
+        document.getElementById('prompt-text').classList.remove('watch-sequence');
+        document.getElementById('prompt-text').innerHTML = "Repeat the sequence.";
+        document.getElementById('prompt-text').classList.add('repeat-sequence');
+    }
+
+    function disableUserClick() {
+        startButton.removeEventListener('click', start, false);
+        document.getElementById('0').removeEventListener('click', userClick, false);
+        document.getElementById('1').removeEventListener('click', userClick, false);
+        document.getElementById('2').removeEventListener('click', userClick, false);
+        document.getElementById('3').removeEventListener('click', userClick, false);
+
+        for (var i = 0; i < tiles.length; i++) {
+            tiles[i].removeEventListener('mousedown', userDown, false);
+            tiles[i].removeEventListener('mouseup', userUp, false);
+        }
+    }
+
+    function userDown(event) {
+        event.target.style.opacity = "1";
+    }
+
+    function userUp(event) {
+        event.target.style.opacity = "0.3";
+    }
+
+})();
